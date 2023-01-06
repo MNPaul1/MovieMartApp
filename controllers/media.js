77f058f41e471fe443f6be551806976e3957540d
MN Paul <87216532+MNPaul@users.noreply.github.com>
2023-01-06 12:27:17 -0800

    All done connected with front end, pagination left

--- controllers/media.js
+++ controllers/media.js
@@ -1,10 +1,9 @@
 const axios = require("axios");
 const asyncHandler = require("../middleware/async");
-
 // @descE   Get all new movies
 //@route    GET /media
 exports.getNewMedia = asyncHandler(async (req, res, next) => {
-  const url = `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.TMDB_API_KEY}a`;
+  const url = `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.TMDB_API_KEY}`;
   const data = await axios.get(url, {
     headers: { "Accept-Encoding": "gzip,deflate,compress" },
   });
@@ -12,7 +11,7 @@ exports.getNewMedia = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
     count: results.length,
-    data: results,
+    data: data.data,
   });
 });
 
@@ -49,26 +48,31 @@ exports.getMetaData = asyncHandler(async (req, res, next) => {
   const {
     genres,
     original_language,
-    original_title,
+    title,
     overview,
     poster_path,
     release_date,
     runtime,
     vote_count,
     vote_average,
+    backdrop_path,
+    tagline
   } = movieMetaData.data;
 
   const movieData = {
-    title: original_title,
+    title: title,
     genres: genres,
     original_language: original_language,
     overview: overview,
+    vote_average: vote_average,
     poster_path: `https://image.tmdb.org/t/p/w500/${poster_path}`,
+    backdrop_path:`https://image.tmdb.org/t/p/w500/${backdrop_path}`,
     release_date: release_date,
     runtime: runtime,
-    rating: Math.round(vote_average / 2),
+    rating: vote_average / 2,
     vote_count: vote_count,
     cast: cast.splice(0, 6),
+    tagline:tagline
   };
   res.status(200).json({
     success: true,
@@ -83,9 +87,11 @@ exports.getAllGenres = asyncHandler(async (req, res, next) => {
   const data = await axios.get(url, {
     headers: { "Accept-Encoding": "gzip,deflate,compress" },
   });
+  const response = data.data;
+  // response.genres.filter(item => delete item['id'])
   res.status(200).json({
     success: true,
-    data: data.data,
+    data: response,
   });
 });
 
@@ -93,7 +99,7 @@ exports.getAllGenres = asyncHandler(async (req, res, next) => {
 //@route     GET /media/movies/:genre
 exports.getMoviesByGenre = asyncHandler(async (req, res, next) => {
   const { genre } = req.params;
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&with_genres=${genre}&with_watch_monetization_types=flatrate`;
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=${genre}&with_watch_monetization_types=flatrate`;
   const data = await axios.get(url, {
     headers: { "Accept-Encoding": "gzip,deflate,compress" },
   });
@@ -107,7 +113,7 @@ exports.getMoviesByGenre = asyncHandler(async (req, res, next) => {
 //@route    GET /media/recommendedmovies/:movie_id
 exports.getRecommendedMovies = asyncHandler(async (req, res, next) => {
   const { movie_id } = req.params;
-  const url = `https://api.themoviedb.org/3/movie/${movie_id}/recommendations?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`;
+  const url = `https://api.themoviedb.org/3/movie/${movie_id}/similar?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`;
   const data = await axios.get(url, {
     headers: { "Accept-Encoding": "gzip,deflate,compress" },
   });
@@ -117,3 +123,34 @@ exports.getRecommendedMovies = asyncHandler(async (req, res, next) => {
     data: data.data,
   });
 });
+//@desc     Get recommended movies
+//@route    GET /media/languages
+exports.getCountries = asyncHandler(async (req, res, next) => {
+  const url = `https://api.themoviedb.org/3/configuration/languages?api_key=${process.env.TMDB_API_KEY}`;
+  const data = await axios.get(url, {
+    headers: { "Accept-Encoding": "gzip,deflate,compress" },
+  });
+  let countries = data.data
+  countries = countries.filter(country => (delete country['name'], country['english_name']!=='No Language'))
+  res.status(200).json({
+    success: true,
+    count: countries.length,
+    data: countries,
+  });
+});
+
+//@desc     Get recommended movies
+//@route    GET /media/languages/:language
+exports.getMoviesByLanguage = asyncHandler(async (req,res,next)=> {
+  const {language} = req.params
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_original_language=${language}&with_watch_monetization_types=flatrate`
+  const data = await axios.get(url, {
+    headers: { "Accept-Encoding": "gzip,deflate,compress" },
+  });
+  let movies = data.data
+  res.status(200).json({
+    success: true,
+    count: movies.length,
+    data: movies,
+  });
+})
--- routes/media.js
+++ routes/media.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const { getNewMedia, getByTitle, getMetaData, getAllGenres, getMoviesByGenre, getRecommendedMovies } = require("../controllers/media");
+const { getNewMedia, getByTitle, getMetaData, getAllGenres, getMoviesByGenre, getRecommendedMovies, getCountries, getMoviesByLanguage } = require("../controllers/media");
 const router = express.Router();
 
 
 router.route("/").get(getNewMedia);
 router.route("/genres").get(getAllGenres);
+router.route("/languages").get(getCountries);
 router.route("/find/:name").get(getByTitle);
 router.route("/:id").get(getMetaData);
 router.route("/movies/:genre").get(getMoviesByGenre);
 router.route("/recommendedmovies/:movie_id").get(getRecommendedMovies);
-
+router.route("/languages/:language").get(getMoviesByLanguage);
 
 module.exports = router;
